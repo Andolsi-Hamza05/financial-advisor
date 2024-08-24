5268f27682926f0838882d8b5de63a8e490cfdd2
Andolsi-Hamza05 <hamza.landolsi@essai.ucar.tn>
2024-08-24 04:33:16 +0100

    Initial setup of data-collection

---
+++ services/data_collection/logger.py
@@ -0,0 +1,30 @@
+import logging
+import os
+from datetime import datetime
+
+
+def setup_logging(level=logging.INFO) -> logging.Logger:
+    """Setup logging configuration to save logs in a 'loggings' directory under a script-specific subfolder."""
+
+    # Get the name of the script that is being executed
+    script_name = os.path.splitext(os.path.basename(__import__('sys').argv[0]))[0]
+
+    # Create the 'loggings' directory and a subfolder named after the script
+    log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'loggings', script_name)
+    os.makedirs(log_dir, exist_ok=True)
+
+    # Create a log file with a timestamp
+    log_filename = os.path.join(log_dir, f"log_data-collection_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
+
+    # Configure logging
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=level,
+        handlers=[
+            logging.FileHandler(log_filename),
+            logging.StreamHandler()
+        ]
+    )
+
+    logger = logging.getLogger(__name__)
+    return logger
