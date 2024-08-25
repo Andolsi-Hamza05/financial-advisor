8944bc845d02aa620fce86cfbb8cff5e8d9fc5ff
Andolsi-Hamza05 <hamza.landolsi@essai.ucar.tn>
2024-08-25 05:23:05 +0100

    developed an api for the data collection microservice with core/dependencies/routers structure

---
+++ services/data_collection/main/dependencies/kafka.py
@@ -0,0 +1,7 @@
+from main.core.gateways.kafka import Kafka
+
+
+def get_kafka_instance():
+    if Kafka.instance:
+        return Kafka.instance
+    return Kafka()
--- services/data_collection/main/main.py
+++ services/data_collection/main/main.py
@@ -1,16 +1,55 @@
-from fastapi import FastAPI
-from .routers import (
-    etfs_router,
-    bond_funds_router,
-    equity_us_funds_router,
-    equity_international_funds_router,
-    stocks_router
+import time
+
+from main.core.gateways.kafka import Kafka
+from main.dependencies.kafka import get_kafka_instance
+from main.enum import EnvironmentVariables
+from main.routers import bond_funds_routers
+
+from dotenv import load_dotenv
+
+from fastapi import Depends, FastAPI, Request
+
+load_dotenv()
+
+
+app = FastAPI(title='Kafka Publisher API')
+kafka_server = Kafka(
+    topic=EnvironmentVariables.KAFKA_TOPIC_NAME.get_env(),
+    port=EnvironmentVariables.KAFKA_PORT.get_env(),
+    servers=EnvironmentVariables.KAFKA_SERVER.get_env(),
 )
 
-app = FastAPI()
 
-app.include_router(etfs_router.router, prefix="/api", tags=["ETFs"])
-app.include_router(bond_funds_router.router, prefix="/api", tags=["Bond Funds"])
-app.include_router(equity_us_funds_router.router, prefix="/api", tags=["Equity US Funds"])
-app.include_router(equity_international_funds_router.router, prefix="/api", tags=["Equity International Funds"])
-app.include_router(stocks_router.router, prefix="/api", tags=["Stocks"])
+async def lifespan(app: FastAPI):
+    # Startup event
+    await kafka_server.aioproducer.start()
+
+    # Shutdown event
+    yield
+
+    await kafka_server.aioproducer.stop()
+
+# Use the lifespan event handler
+app = FastAPI(title='Kafka Publisher API', lifespan=lifespan)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+@app.get('/')
+def get_root():
+    return {'message': 'API is running...'}
+
+
+app.include_router(
+    bond_funds_routers.router,
+    prefix="/producer",
+    tags=["producer"],
+    dependencies=[Depends(get_kafka_instance)],
+)
