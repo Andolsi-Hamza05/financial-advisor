23e61107b5a39184ffeae04707303f91f773966d
Andolsi-Hamza05 <hamza.landolsi@essai.ucar.tn>
2024-09-21 23:53:15 +0100

    Ingested raw data as bronze layer

--- services/feature_selection/build.sh
+++
@@ -1,9 +0,0 @@
-# Define variables
-IMAGE_NAME="hamzalandolsi/feature_selection"
-TAG="latest"
-
-# Build the Docker image
-docker build -t ${IMAGE_NAME}:${TAG} .
-
-# Push the Docker image to Docker Hub
-docker push ${IMAGE_NAME}:${TAG}
--- services/feature_selection/silver/__main__.py
+++
@@ -1,9 +0,0 @@
-from silver import initialize_spark, process_funds_data
-
-if __name__ == "__main__":
-    spark = initialize_spark()
-    file_path = "Equity_US_Index_Funds_data.json"
-
-    equity_us_index_funds = process_funds_data(file_path, spark)
-
-    equity_us_index_funds.show(truncate=False)
