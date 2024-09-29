12105b98a86d7e85925297072c9d708d42fb43a7
Andolsi-Hamza05 <hamza.landolsi@essai.ucar.tn>
2024-09-29 18:05:58 +0100

    Enhanced format of output to fix tool calling problems

--- services/customer_profiling/smart/structured_output.py
+++ services/customer_profiling/smart/structured_output.py
@@ -5,14 +5,14 @@ class CheckSpecificMesurableTimebound(BaseModel):
     """Check if the goal is specific, measurable and time specified"""
 
     goal: str = Field(description="The clearly defined goal of the investor, if not provided return an empty string")
-    initial_investment: float = Field(description="The initial amount invested (upfront)")
-    monthly_contribution: float = Field(description="The amount of money that the investor is willing to invest periodically(monthly/annually)")
-    annual_return: float = Field(description="Expected annual return rate (r)")
-    target_value: float = Field(description="The target future value that the investor wants to achieve.")
-    time_horizon: int = Field(description="The time horizon for the investment.")
-    is_specific: str = Field(description="True if the investor's goal is well defined and specified, else False.")
-    is_time: str = Field(description="True if the investor's time horizon is specified, else False.")
-    is_measurable: str = Field(description="True if the investor specifies the initial_investment, monthly_contribution and target_value, else False.")
+    initial_investment: float = Field(default=0.0, description="The initial amount invested (upfront)")
+    monthly_contribution: float = Field(default=0.0, description="The amount of money that the investor is willing to invest periodically(monthly/annually)")
+    annual_return: float = Field(default=0.0, description="Expected annual return rate (r)")
+    target_value: float = Field(default=0.0, description="The target future value that the investor wants to achieve.")
+    time_horizon: int = Field(default=0, description="The time horizon for the investment.")
+    is_specific: str = Field(default="False", description="True if the investor's goal is well defined and specified, else False.")
+    is_time: str = Field(default="False", description="True if the investor's time horizon is specified, else False.")
+    is_measurable: str = Field(default="False", description="True if the investor specifies the initial_investment, monthly_contribution and target_value, else False.")
 
 
 class CheckAchievable(BaseModel):
